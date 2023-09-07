890375cec3043348dcc9ed7b7c1cf6f41793cb9c
jonathanho168 <62679754+jonathanho168@users.noreply.github.com>
2023-09-07 16:18:28 -0400

    () around lambda function input

--- src/user/create.ts
+++ src/user/create.ts
@@ -217,7 +217,7 @@ export default function (User : UserMethods) : void {
                 subject: `[[email:welcome-to, ${meta.config.title as string || meta.config.browserTitle as string || 'NodeBB'}]]`,
             // The next line calls a function in a module that has not been updated to TS yet
             // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            }).catch(err => {
+            }).catch((err) => {
                 if (err instanceof Error) {
                     winston.error(`[user.create] Validation email failed to send\n[emailer.send] ${err.stack}`);
                 }
